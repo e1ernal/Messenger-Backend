412110d9a6f4854575298b4056f843506cfd2762
unknown <razarenov.aleksandar@yandex.ru>
2024-02-23 22:12:29 +0300

    fix bug on search

--- Backend/api/services/user/search.py
+++ Backend/api/services/user/search.py
@@ -1,12 +1,13 @@
 from django import forms
+from service_objects.fields import ModelField
 from service_objects.services import Service
 
 from models_app.models import User
 
 
 class UserSearchService(Service):
-
     search = forms.CharField()
+    user = ModelField(User)
 
     def process(self):
         return self.get_users
@@ -15,4 +16,6 @@ class UserSearchService(Service):
     def get_users(self):
         return User.objects.filter(
             username__istartswith=self.cleaned_data['search']
+        ).exclude(
+            username=self.cleaned_data['user'].username
         )
