5ae62814d7a299ecb94048fd95f35709dbe7fb75
unknown <>
2024-03-29 21:04:00 +0300

    try to fix timestamp to real date

--- Backend/api/serializers/direct_chats/serializers.py
+++ Backend/api/serializers/direct_chats/serializers.py
@@ -25,15 +25,15 @@ class DirectChatSerializer(serializers.ModelSerializer):
 
 class DirectChatListSerializer(serializers.ModelSerializer):
     last_message = serializers.CharField(source='last_message.text', default=None)
-    last_message_created = serializers.SerializerMethodField()
+    last_message_created = serializers.DateTimeField(source='last_message.created_at')
     direct_id = serializers.IntegerField()
 
-    def get_last_message_created(self, obj):
-        if obj.last_message:
-            datetime_obj = datetime.datetime.combine(obj.last_message.created_at, datetime.time.min)
-            unix_timestamp = int(datetime_obj.timestamp())
-            return unix_timestamp
-        return None
+    # def get_last_message_created(self, obj):
+    #     if obj.last_message:
+    #         datetime_obj = datetime.datetime.combine(obj.last_message.created_at, datetime.time.min)
+    #         unix_timestamp = int(datetime_obj.timestamp())
+    #         return unix_timestamp
+    #     return None
 
     class Meta:
         model = User
--- Backend/api/serializers/messages/serializers.py
+++ Backend/api/serializers/messages/serializers.py
@@ -7,18 +7,18 @@ from models_app.models import Message
 
 class MessageSerializer(serializers.ModelSerializer):
     author = UserUpdateSerializer()
-    created_at = serializers.SerializerMethodField()
-    updated_at = serializers.SerializerMethodField()
-
-    def get_created_at(self, obj):
-        datetime_obj = datetime.datetime.combine(obj.created_at, datetime.time.min)
-        unix_timestamp = int(datetime_obj.timestamp())
-        return unix_timestamp
-
-    def get_updated_at(self, obj):
-        datetime_obj = datetime.datetime.combine(obj.updated_at, datetime.time.min)
-        unix_timestamp = int(datetime_obj.timestamp())
-        return unix_timestamp
+    # created_at = serializers.SerializerMethodField()
+    # updated_at = serializers.SerializerMethodField()
+    #
+    # def get_created_at(self, obj):
+    #     datetime_obj = datetime.datetime.combine(obj.created_at, datetime.time.min)
+    #     unix_timestamp = int(datetime_obj.timestamp())
+    #     return unix_timestamp
+    #
+    # def get_updated_at(self, obj):
+    #     datetime_obj = datetime.datetime.combine(obj.updated_at, datetime.time.min)
+    #     unix_timestamp = int(datetime_obj.timestamp())
+    #     return unix_timestamp
 
     class Meta:
         model = Message
