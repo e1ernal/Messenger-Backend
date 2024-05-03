0c5445c5c7fdd7444d7a2394bf96e076612111a4
unknown <>
2024-05-03 21:18:26 +0300

    added env variable

--- Backend/conf/settings.py
+++ Backend/conf/settings.py
@@ -10,21 +10,28 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
+import environ
+
 from pathlib import Path
+
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i4kamlg7#aubk!wh+=&eb4@f-408c%kab0(7w%2*jir1@q+nku'
+SECRET_KEY = env("SECRET_KEY", cast=str)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = env("ALLOWED_HOSTS", cast=lambda v: v.split())
 
 # Application definition
 
@@ -79,10 +86,15 @@ ASGI_APPLICATION = "conf.asgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env("NAME", cast=str),
+        'USER': env("USER", cast=str),
+        'PASSWORD': env("PASSWORD", cast=str),
+        'HOST': env("HOST", cast=str),
+        'PORT': env("PORT", cast=int, default=5432),
     }
 }
 
